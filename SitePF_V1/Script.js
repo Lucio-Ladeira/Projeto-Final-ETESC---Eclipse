fb1de61c7d82d4aca71feddeff2fd66122daa602
Kksaimo <saymonsantos433@gmail.com>
2026-06-28 17:28:18 -0300

    Algumas coisas no homepage pra adiantar o projeto
    Base banner ovmd
    Base barra de pesquisa ovmd
    Base botões conta e carrinho ovmd
    Ícone pros botões conta, carrinho e trocar tema ovmd
    Base Footer(é só o footer do site de apresentação) ovmd
    "Builds" no nav ovmd
    
    ovmd = obviamente vamos mudar depois

--- SitePF_V1/Script.js
+++ SitePF_V1/Script.js
@@ -6,3 +6,17 @@ function switchTheme() {
         body.classList.replace("tema-light", "tema-dark")
     }
 }
+
+const botao = document.getElementById("perfilBtn");
+const menu = document.getElementById("menuPerfil");
+
+botao.addEventListener("click", () => {
+    menu.style.display =
+        menu.style.display === "block" ? "none" : "block";
+});
+
+document.addEventListener("click", function(event) {
+    if (!event.target.closest(".perfil")) {
+        menu.style.display = "none";
+    }
+});
